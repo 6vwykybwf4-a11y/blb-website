928cdedad75a918c3869fcb88731a576c226daba
6vwykybwf4-a11y <6vwykybwf4@privaterelay.appleid.com>
2026-04-23 17:04:58 -0500

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'BLB Website',
+  description: 'Brothers Legacy Blueprint',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
